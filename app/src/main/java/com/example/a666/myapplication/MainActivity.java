8a2cbf90006ea4dd5ada0c12d22b018459d3d1f3
Administrator <1363239411@qq.com>
2017-12-05 16:32:09 +0800

    woshin-menghaodong

--- app/src/main/java/com/example/a666/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a666/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //和HAHA
         //ceshi  ceshi ceshi
     }
 }
